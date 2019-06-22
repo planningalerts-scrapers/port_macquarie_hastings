2db0b210d0837758a58a0c2c01e8cecff54d7226
Matthew Landauer <mlandauer@gmail.com>
2019-06-22 13:53:11 +1000

    Update to latest version of the gems

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,3 @@
 require "masterview_scraper"
 
-MasterviewScraper.scrape_and_save_period(
-  url: "https://datracker.pmhc.nsw.gov.au",
-  period: :last30days,
-  use_api: true
-)
+MasterviewScraper.scrape_and_save(:port_macquarie_hastings)
